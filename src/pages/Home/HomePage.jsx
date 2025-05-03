112c4c2b9d8e8ec5b3095abab9f61f5f5fc58c78
Ananda Mux <147627901+anandamux@users.noreply.github.com>
2025-05-03 11:32:54 -0300

    estilização da página inicial

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,28 +1,35 @@
+// Aqui é o "html" de um projeto react, ele sempre deve ser retornado pelo componente e sempre tem q ter uma div pai
+// que encapsula todo esse html, nesse caso, HomePage-Container é esse cara, se vc colocar uma outra div no mesmo nivel,
+// ou seja, fora dessa div, terá um erro. Dentro do HomePage-Container pode mudar a estrutura como quiser para ajustar o estilo.
+
+// Outro adento é que quando for adicionar uma classe em um elemento aqui em react usamos className em vez de class como no HTML comum.
 
 import { useNavigate } from "react-router-dom";
-
-import Logo from "@assets/logo.png"
+import "./HomePage.css";
+import Logo from "@assets/logo.png";
 
 const HomePage = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  return (
+    <div className="HomePage-Container">
+      <img className="img-logo" src={Logo} alt="logo" />
 
-    // Aqui é o "html" de um projeto react, ele sempre deve ser retornado pelo componente e sempre tem q ter uma div pai 
-    // que encapsula todo esse html, nesse caso, HomePage-Container é esse cara, se vc colocar uma outra div no mesmo nivel, 
-    // ou seja, fora dessa div, terá um erro. Dentro do HomePage-Container pode mudar a estrutura como quiser para ajustar o estilo.
+      <button
+        className="btn-new-form"
+        onClick={() => navigate("./form-creation")}
+      >
+        Novo questionário
+      </button>
 
-    // Outro adento é que quando for adicionar uma classe em um elemento aqui em react usamos className em vez de class como no HTML comum.
+      <button
+        className="btn-access-form"
+        onClick={() => navigate("./questionarios")}
+      >
+        Acessar questionários
+      </button>
+    </div>
+  );
+};
 
-
-    return (
-        <div className="HomePage-Container">
-            
-            <img className="img-logo" src={Logo} alt="logo"/>
-
-            <button className="btn-new-form" onClick={() => navigate("./form-creation")}>Novo Questionário</button>
-
-        </div>
-    )
-}
-
-export default HomePage
+export default HomePage;
