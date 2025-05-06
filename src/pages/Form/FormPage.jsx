92a48414e8f254f0a030e21e1333bf80d8488cd9
IgorFBastos <igordrbastos@gamil.com>
2025-05-06 17:46:30 -0300

    salvando mudanças de estilo

--- src/pages/Form/FormPage.jsx
+++ src/pages/Form/FormPage.jsx
@@ -1,22 +1,64 @@
 import { UNSAFE_decodeViaTurboStream, useSearchParams } from 'react-router-dom';
 
+import { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle as faCircleSolid } from '@fortawesome/free-solid-svg-icons'
+import { faCircle as faCircleRegular } from '@fortawesome/free-regular-svg-icons'
+
+import "./FormPage.css";
+
+
+
 const FormPage = () => {
     const [searchParams] = useSearchParams();
 
-    const questions = JSON.parse(searchParams.get('questions') || '[]');
+    const initialQuestions = JSON.parse(searchParams.get('questions') || '[]');
     const title = searchParams.get('title') || '';
     const medic = searchParams.get('medic') || '';
     const patient = searchParams.get('patient') || '';
 
+
+    const [questions, setQuestions] = useState(initialQuestions);
+
+
+    console.log("questions: ", questions)
+
+
     const afirmativeQuestion = (question, i) => {
+
+
+        const handleCheckboxQuestionYES = () => {
+            const updated = [...questions];
+            updated[index].response = { yes: true, no: false };
+            setQuestions(updated);
+        };
+
+        const handleCheckboxQuestionNO = () => {
+            const updated = [...questions];
+            updated[index].response = { yes: false, no: true };
+            setQuestions(updated);
+        };
+
         return (
-            <div className="question afirmative-question">
-                <p>{i}. {question}</p>
-                <div>
-                    sim
-                </div>
-                <div>
-                    não
+
+            <div className="questionCard afirmative-question">
+                <p className="questionNumber">{i}.</p>
+
+                <div className="question-response-container">
+
+                    <p className="question">{question}</p>
+                    <div className="afirmative-input">
+                        <div onClick={handleCheckboxQuestionYES}>
+                            <FontAwesomeIcon icon={faCircleSolid} className="fa-icon" />
+                            sim
+                        </div>
+                        <div onClick={handleCheckboxQuestionNO}>
+                            <FontAwesomeIcon icon={faCircleRegular} className="fa-icon" />
+                            não
+                        </div>
+                    </div>
+
                 </div>
             </div>
         )
@@ -24,10 +66,14 @@ const FormPage = () => {
 
     const descriptiveQuestion = (question, i) => {
 
-        return(
-            <div className="quastion descriptive-question">
-                <p>{i}. {question}</p>
-                <input type="text" />
+        return (
+
+            <div className="questionCard descriptive-question">
+                <p className="questionNumber">{i}.</p>
+                <div className="question-response-container">
+                    <p className="question">{question}</p>
+                    <textarea className="description-input" name="" id="" placeholder="descreva sua resposta"></textarea>
+                </div>
             </div>
         )
     }
@@ -36,40 +82,54 @@ const FormPage = () => {
     const numericQuestion = (question, i) => {
 
         return (
-            <div className="question numeric-question">
-                <p>{i}. {question}</p>
-                <input type="number" />
+            <div className="questionCard numeric-question">
+                <p className="questionNumber">{i}.</p>
+                <div className="question-response-container">
+                    <p className="question">{question}</p>
+                    <input className="number-input" type="number" placeholder="digite sua resposta" />
+                </div>
             </div>
         )
     }
 
     return (
-        <div>
-            <h1>{title}</h1>
-            <p>Médico (a): {medic}</p>
-            <p>Paciente: {patient}</p>
-
-            {/* Renderiza as perguntas aqui */}
-            {questions.map((q, i) => {
-                
-                console.log(q)
-                if(q.type === "afirmative") {
-                    return afirmativeQuestion(q.question, i);
-                }
-
-                if(q.type === "descriptive") {
-                    return descriptiveQuestion(q.question, i);
-                }
-                
-                if(q.type === "numeric") {
-                    return numericQuestion(q.question, i);
-                }
-            })}
+        <div className="form-response-container">
 
 
-            <div className="btn-submit-form">
-                <button>Enviar Questionário</button>
+
+            <div className="header-form">
+                <h1 className="form-title">{title}</h1>
+                <p className="form-medic">Médico (a): {medic}</p>
+                <p className="form-patient">Paciente: {patient}</p>
+
             </div>
+
+            <div className="questionCards-container">
+                {/* Renderiza as perguntas aqui */}
+                {questions.map((q, i) => {
+
+                    console.log(q)
+                    if (q.type === "afirmative") {
+                        return afirmativeQuestion(q.question, i + 1);
+                    }
+
+                    if (q.type === "descriptive") {
+                        return descriptiveQuestion(q.question, i + 1);
+                    }
+
+                    if (q.type === "numeric") {
+                        return numericQuestion(q.question, i + 1);
+                    }
+                })}
+
+            </div>
+
+            {/* {questions.lenght && ( */}
+                <div className="btn-submit-form">
+                    <button>Enviar Questionário</button>
+                </div>
+            {/* )} */}
+
         </div>
     );
 };
