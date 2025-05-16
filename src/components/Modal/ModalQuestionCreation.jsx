732a2baf1b0fa982ace3aa2b349861646bdb9d69
Ananda Mux <147627901+anandamux@users.noreply.github.com>
2025-05-16 12:50:40 -0300

    git checkout dev estilizacao inicial da criação do questionário!

--- src/components/Modal/ModalQuestionCreation.jsx
+++ src/components/Modal/ModalQuestionCreation.jsx
@@ -1,125 +1,123 @@
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
-import { useEffect, useState } from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import "./Modal.css";
 
+const ModalQuestionCreation = ({
+  onClose,
+  onCreateQuestion,
+  onEditQuestion,
+  existingQuestion,
+}) => {
+  const [isNumeric, setIsNumeric] = useState(false);
 
+  const [questionText, setQuestionText] = useState("");
+  const [questionType, setQuestionType] = useState("afirmative");
+  const [questionFormula, setQuestionFormula] = useState(null);
 
-import "./Modal.css"
+  useEffect(() => {
+    if (existingQuestion) {
+      setQuestionText(existingQuestion.question);
+      setQuestionType(existingQuestion.type);
+      setIsNumeric(existingQuestion.type === "numeric");
+    }
+  }, [existingQuestion]);
 
+  const handleTypeChange = (e) => {
+    const selected = e.target.value;
 
-const ModalQuestionCreation = ({ onClose, onCreateQuestion, onEditQuestion, existingQuestion }) => {
+    setQuestionType(selected);
 
-    const [isNumeric, setIsNumeric] = useState(false);
+    if (selected === "numeric") {
+      setIsNumeric(true);
+    } else {
+      setIsNumeric(false);
+    }
+  };
 
-    const [questionText, setQuestionText] = useState("");
-    const [questionType, setQuestionType] = useState("afirmative");
-    const [questionFormula, setQuestionFormula] = useState(null);
+  const handleSave = () => {
+    if (!questionText) {
+      alert("Digite uma pergunta!");
+      return;
+    }
 
-    useEffect(() => {
-        if (existingQuestion) {
-            setQuestionText(existingQuestion.question);
-            setQuestionType(existingQuestion.type);
-            setIsNumeric(existingQuestion.type === 'numeric');
-        }
-    }, [existingQuestion]);
-
-    const handleTypeChange = (e) => {
-        const selected = e.target.value;
-
-        setQuestionType(selected);
-
-        if (selected === 'numeric') {
-            setIsNumeric(true);
-
-        } else {
-            setIsNumeric(false)
-        }
+    const newQuestion = {
+      question: questionText,
+      type: questionType,
+      formula: questionType === "numeric" ? questionFormula || null : null,
+      response: getInitialResponse(questionType),
     };
 
-    const handleSave = () => {
+    if (existingQuestion) {
+      onEditQuestion(newQuestion);
+    } else {
+      onCreateQuestion(newQuestion);
+    }
+  };
 
-        if (!questionText) {
-            alert("Digite uma pergunta!");
-            return;
-        }
+  const getInitialResponse = (type) => {
+    if (type === "afirmative") {
+      return { yes: true, no: false };
+    }
 
-        const newQuestion = {
-            question: questionText,
-            type: questionType,
-            formula: questionType === "numeric" ? questionFormula || null : null,
-            response: getInitialResponse(questionType)
-        };
+    if (type === "descriptive") {
+      return { text: "" };
+    }
 
-        if (existingQuestion) {
-            onEditQuestion(newQuestion);
-        } else {
-            onCreateQuestion(newQuestion);
-        }
-    };
+    if (type === "numeric") {
+      return { numeric: null, processed: null };
+    }
 
-    const getInitialResponse = (type) => {
-        if (type === "afirmative") {
-            return { yes: true, no: false };
-        }
-    
-        if (type === "descriptive") {
-            return { text: "" };
-        }
-    
-        if (type === "numeric") {
-            return { numeric: null, processed: null };
-        }
-    
-        return null;
-    };
+    return null;
+  };
 
-    return (
-        <div className="modal-overlay">
-
-            <div className="Modal-container">
-                <FontAwesomeIcon icon={faXmark} onClick={() => onClose(false)} />
-                <div className="question-container">
-                    <h2>Pergunta:</h2>
-                    <textarea
-                        rows="4"
-                        placeholder="Digite a pergunta aqui..."
-                        onChange={(e) => setQuestionText(e.target.value)}
-                    />
-                </div>
-
-                <div className="questionType-container">
-                    <h2>Tipo da Resposta:</h2>
-                    <select onChange={handleTypeChange}>
-                        <option value="afirmative">Afirmativa</option>
-                        <option value="descriptive">Descritiva</option>
-                        <option value="numeric">Numérica</option>
-                    </select>
-                </div>
-
-                {isNumeric && (
-                    <div className="formula-container">
-
-                        <div className="input-container">
-                            <input type="text" placeholder="Digite uma fórmula se necessário" />
-                        </div>
-
-                        <div>
-                            {/* Aqui vai o código para exibir a calculadora de formulas  */}
-                            Aqui teremos a calculadora que será implementada futuramente
-                        </div>
-                    </div>
-                )}
-
-                <div className="btn-create-question-container">
-                    <button onClick={() => handleSave()}>
-                        {existingQuestion ? "Salvar Alterações" : "Criar"}
-                    </button>
-                </div>
-
-            </div>
+  return (
+    <div className="modal-overlay">
+      <div className="Modal-container">
+        <FontAwesomeIcon icon={faXmark} onClick={() => onClose(false)} />
+        <div className="question-container">
+          <h2>Pergunta:</h2>
+          <textarea
+            rows="4"
+            placeholder="Digite a pergunta aqui..."
+            onChange={(e) => setQuestionText(e.target.value)}
+          />
         </div>
-    )
-}
+
+        <div className="questionType-container">
+          <h2>Tipo da Resposta:</h2>
+          <select onChange={handleTypeChange} value={questionType}>
+            <option value="afirmative">Afirmativa</option>
+            <option value="descriptive">Descritiva</option>
+            <option value="numeric">Numérica</option>
+          </select>
+        </div>
+
+        {isNumeric && (
+          <div className="formula-container">
+            <div className="input-container">
+              <input
+                type="text"
+                placeholder="Digite uma fórmula se necessário"
+              />
+            </div>
+
+            <div>
+              {/* Aqui vai o código para exibir a calculadora de formulas  */}
+              Aqui teremos a calculadora que será implementada futuramente
+            </div>
+          </div>
+        )}
+
+        <div className="btn-create-question-container">
+          <button onClick={() => handleSave()}>
+            {existingQuestion ? "Salvar Alterações" : "Criar"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ModalQuestionCreation;
